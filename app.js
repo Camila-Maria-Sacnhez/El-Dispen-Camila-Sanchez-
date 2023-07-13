3657f5ae2fc02666953f25936fa6cf493ec8c7a8
el <camilamariasanchez@gmail.com>
2023-07-13 19:06:22 -0300

    tercer entrega

--- app.js
+++ app.js
@@ -1,117 +1,174 @@
-let nombre;
-
-function cliente(nombrecliente) {
-
-    alert('Hola' + ' ' + nombrecliente + ' ' + 'Biendenid@ al El dispensario');
-}
-
-nombre = prompt('Ingrese su nombre:');
-cliente(nombre);
-
-console.log('una persona esta visitando nuestra pagina');
 
 
 
-let continuar = 'si';
-let total = 0;
-let precio;
-let cantidad;
-let item;
+let carrito = [];
 
 
-function producto(nombre, precio) {
+function producto(nombre, precio, cant) {
     this.nombre = nombre;
     this.precio = precio;
+    this.cantidad = cant;
 }
 
-const Opcion1 = new producto('Oleos maserados', 1500)
-const Opcion2 = new producto('Amuletos', 2300)
-const Opcion3 = new producto('Posimas', 2000)
-const Opcion4 = new producto('Sahumos', 2500)
 
-alert(
-    'Tenemos los siguientes productos en nuestro catalogo' + '\n' +
-    'Opcion 1:' + ' ' + Opcion1.nombre + ' ' + '$' + Opcion1.precio + '\n' +
-    'Opcion 2:' + ' ' + Opcion2.nombre + ' ' + '$' + Opcion2.precio + '\n' +
-    'Opcion 3:' + ' ' + Opcion3.nombre + ' ' + '$' + Opcion3.precio + '\n' +
-    'Opcion 4:' + ' ' + Opcion4.nombre + ' ' + '$' + Opcion4.precio
+Sahumos = new producto('Sahumo', 1500, 0)
+Desodorante = new producto('Desodorante Natural', 2300, 0)
+Tintura = new producto('Tintura', 2000, 0)
 
-)
-console.log('Se le mostro el catalogo al cliente')
+carrito.push(Sahumos)
+carrito.push(Desodorante)
+carrito.push(Tintura)
 
-
-
-
-
-const carrito = []
-
-function ingresoProducto(items) {
-    carrito.push(items);
-
+function guardarCarrito() {
+    localStorage.setItem('carrito', JSON.stringify(carrito));
 }
 
-function itemsEnCarrito() {
-    do {
-        console.log('El cliente esta agregando  items al carrito de compra');
-        items = prompt('Indique el nombre del producto que desea')
-        ingresoProducto(items);
-        continuar = prompt('Desea agregar otro item en el carrito de compras? Si o No');
 
-
-    } while (continuar == 'si')
-}
-
-function verItems() {
-    for (let i = 0; i < carrito.length; i++) {
-        console.log(carrito[i]);
+function recuperarCarrito() {
+    const carritoGuardado = localStorage.getItem('carrito');
+    if (carritoGuardado) {
+        carrito = JSON.parse(carritoGuardado);
     }
-
 }
 
+/// tabla 
 
-itemsEnCarrito();
-verItems()
-
-function buscarProductoEnCarrito(items) {
-    return carrito.includes(items);
-}
-
-const productoBusqueda = prompt('Ingrese nombre de producto que quiere corroborar:');
-const estaEnCarrito = buscarProductoEnCarrito(productoBusqueda);
-
-if (estaEnCarrito) {
-    console.log(`El producto "${productoBusqueda}" está en el carrito.`);
-    alert('El producto si esta en el carrito')
-} else {
-    console.log(`El producto "${productoBusqueda}" no está en el carrito.`);
-    alert('El prodcuto no esta en el carrito')
-}
-
-function verificarElementoEnCarrito(condicion) {
-    return carrito.some(item => {
-
-        return item == "sahumos"
+function dibujarTabla() {
+    const bodyTabla = document.getElementById('item');
+    bodyTabla.innerHTML = ``;
+    carrito.forEach((item, index) => {
+        bodyTabla.innerHTML = bodyTabla.innerHTML +
+            `<tr>
+                        <th>${index + 1}</th>
+                        <td>${item.nombre}</td>
+                        <td>${item.cantidad}</td>
+                        <td>${item.precio}</td>
+                    </tr>
+        `;
     });
+
+    calcularTotal();
 }
 
 
+/// calcular  tabla 
 
-function condicionPrecioMayor50(item) {
-    return item == "sahumos";
+
+function calcularTotal() {
+
+    let total = 0;
+
+    carrito.forEach((item, index) => {
+        total = total + (item.cantidad * item.precio);
+    });
+
+    console.log("Total en :" + total);
+
+    const el_total = document.getElementById('total');
+    el_total.innerHTML = total;
+
 }
 
-const existeProductoCaro = verificarElementoEnCarrito(condicionPrecioMayor50);
 
-if (existeProductoCaro) {
-    console.log('Hay al menos un Sahumo  en el carrito.');
-} else {
-    console.log('No se encontraron Sahumos con este nombre  en el carrito.');
+//////// boton sahumos /////
+
+/// sumar
+
+const BotonSahumos = document.getElementById('BotonSahumos');
+
+function mostrarMensajeSahumo() {
+    Sahumos.cantidad = Sahumos.cantidad + 1;
+    console.log("Agregar sahumos cantidad = " + Sahumos.cantidad);
+    dibujarTabla();
+    
+
 }
 
 
-function gracias() {
-    alert('Gracias por tu compra');
+BotonSahumos.addEventListener('click', mostrarMensajeSahumo);
+
+///// quietar 
+const BotonSahumosDisminuir = document.getElementById('BotonSahumosDisminuir');
+
+function disminuirSahumo() {
+
+    if (Sahumos.cantidad != 0)
+        Sahumos.cantidad = Sahumos.cantidad - 1;
+
+    console.log("Disminuir sahumos cantidad = " + Sahumos.cantidad)
+    dibujarTabla();
+    
+
 }
 
-gracias();
+BotonSahumosDisminuir.addEventListener('click', disminuirSahumo);
+
+///// Boton de desodorante ////////
+
+/// suma 
+
+const BotonDesodorantes = document.getElementById('BotonDesodorantes');
+
+function mostrarMensajeDesodorante() {
+    Desodorante.cantidad = Desodorante.cantidad + 1;
+    console.log("Se agrega Desodorante cantidad = " + Desodorante.cantidad);
+    dibujarTabla();
+    
+}
+
+
+BotonDesodorantes.addEventListener('click', mostrarMensajeDesodorante);
+
+const BotonDesodorantesDisminuir = document.getElementById('BotonDesodorantesDisminuir');
+
+/// QUITAR 
+
+function disminuirDesodorante() {
+
+
+    if (Desodorante.cantidad != 0)
+        Desodorante.cantidad = Desodorante.cantidad - 1
+    console.log("Se restaron desodorantes cantidad = " + Desodorante.cantidad)
+    dibujarTabla();
+    
+}
+
+BotonDesodorantesDisminuir.addEventListener('click', disminuirDesodorante);
+
+////// Boton de tinturas 
+
+///  sumar
+const BotonTinturas = document.getElementById('BotonTinturas');
+
+function mosotarMensajeTintura() {
+    Tintura.cantidad = Tintura.cantidad + 1;
+    console.log("Se agrega Tintura madre  cantidad = " + Tintura.cantidad);
+    dibujarTabla();
+    
+}
+
+
+BotonTinturas.addEventListener('click', mosotarMensajeTintura);
+
+/// quitar 
+
+const BotonTinturasDisminuir = document.getElementById('BotonTinturasDisminuir');
+
+function disminuirTintura() {
+
+
+    if (Tintura.cantidad != 0)
+        Tintura.cantidad = Tintura.cantidad - 1;
+
+    console.log("Se restaron tinturas cantidad = " + Tintura.cantidad)
+    dibujarTabla();
+    
+}
+
+BotonTinturasDisminuir.addEventListener('click', disminuirTintura);
+
+
+
+
+////////////////////////////////////////////
 
